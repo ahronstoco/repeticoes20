86cbab7505f1af2c293d0b69eadb158f29ef5660
Ahron <Ahron@10.146.193.6>
2025-04-15 10:54:00 -0300

    Ex. de repetição

---
+++ src/main/java/com/mycompany/repeticoes20/Repeticoes20.java
@@ -0,0 +1,14 @@
+package com.mycompany.repeticoes20;
+
+/**
+ *
+ * @author Ahron
+ */
+import javax.swing.JOptionPane;
+public class Repeticoes20 {
+ public static void main(String[] args) {
+     for (int i = 1; i <= 10; i++){
+     JOptionPane.showMessageDialog(null,"O valor de i é: " + i);
+     }
+    }
+}
